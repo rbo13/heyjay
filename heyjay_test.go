5bf8eaf9d91940544ff4153d1873b5f4736ca002
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-06-28 20:57:24 +0800

    Added test on as web server using httptest

--- heyjay_test.go
+++ heyjay_test.go
@@ -1,6 +1,10 @@
 package heyjay_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
@@ -13,11 +17,34 @@ type User struct {
 	Address  string
 }
 
-func TestHeyJay(t *testing.T) {
+func TestHeyJayOnCLI(t *testing.T) {
 	user := User{Fullname: "Richard Burk", Age: 22, Address: "Cebu City"}
 	err := heyjay.JSON(os.Stdout, user)
 
 	if err != nil {
 		t.Errorf("Error due to: %v", err)
 	}
+}
+
+func TestHeyJayOnWebServer(t *testing.T) {
+	user := User{Fullname: "Richard Burk", Age: 22, Address: "Cebu City"}
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		heyjay.JSON(w, user)
+	}
+
+	req := httptest.NewRequest("GET", "http://localhost:3030", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+	// Output:
+	// 200
+	// application/json; charset=utf-8
 }
